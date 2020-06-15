ba698bf1338b8e69beae11c7084c0f34ad2909e2
Kishore S <30261988+kishoreio@users.noreply.github.com>
2020-06-15 19:01:57 +0530

    Updates

---
+++ precache-manifest.598432cb049c8d8b22b38d04ec3d5039.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc278f61d6596c1f768b07b3c217112",
+    "url": "/betadesk/index.html"
+  },
+  {
+    "revision": "ae30e297df64ed353f2a",
+    "url": "/betadesk/static/css/main.f2f20fe4.chunk.css"
+  },
+  {
+    "revision": "20c6ac61d7f33edadf04",
+    "url": "/betadesk/static/js/2.d2bc1717.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/betadesk/static/js/2.d2bc1717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae30e297df64ed353f2a",
+    "url": "/betadesk/static/js/main.78188b1e.chunk.js"
+  },
+  {
+    "revision": "c3b268f327958f6ed8df",
+    "url": "/betadesk/static/js/runtime-main.abaa4f76.js"
+  },
+  {
+    "revision": "540cc743d9756a5b5ce3706249766163",
+    "url": "/betadesk/static/media/background.540cc743.png"
+  },
+  {
+    "revision": "6ba09fbc2b92bb49117771f9b082d944",
+    "url": "/betadesk/static/media/logo-small.6ba09fbc.png"
+  },
+  {
+    "revision": "3a28f09b3f055989e5249a5ceacfd29e",
+    "url": "/betadesk/static/media/logo.3a28f09b.png"
+  },
+  {
+    "revision": "4ce91f619de3c63912c6f453e5a8a981",
+    "url": "/betadesk/static/media/overview.4ce91f61.png"
+  }
+]);
